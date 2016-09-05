70c0551a39b7d0e396b1f25a0c46c4055cb6c0a3
Ryan <rsul.dev@me.com>
2016-09-05 17:32:29 -0400

    Add Alfred UID support and update URLs
    Genius doesn't actually support HTTPS yet apparently, since all song
    URLs are redirecting me back to HTTP. But this doesn't break anything
    and hopefully it will work in the future.

--- filter-search.py
+++ filter-search.py
@@ -3,6 +3,7 @@
 import sys, os
 import requests
 import json
+from urllib.parse import urlparse
 
 def main(argv):
     query = argv[1]
@@ -24,11 +25,17 @@ def main(argv):
 
         hit_result = hit['result']
 
+        # At the moment Genius is not returning secure URLs
+        # Also, at the moment these HTTPS urls all redirect back to HTTP
+        # so this is kind of useless. But hopefully that changes.
+        hit_url = urlparse(hit_result['url'])._replace(scheme='https').geturl()
+
         alfred_items.append({
+            "uid": 'https://api.genius.com{}'.format(hit_result['api_path']),
             "title": hit_result['title'],
             "subtitle": hit_result['primary_artist']['name'],
-            "arg": hit_result['url'],
-            "quicklookurl": hit_result['url']
+            "arg": hit_url,
+            "quicklookurl": hit_url
         })
 
     print(json.dumps({
