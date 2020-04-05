ae6517a8813445665b0c7bfc2828db847d6f3067
Ryan <rsul.dev@me.com>
2020-04-05 19:34:41 -0400

    Fix bugs in get-song.js
    This fixes 2 bugs in get-song.js:
    
    1) Adds long-missing try/catch blocks when testing each application.
    
       Some applications may not be installed, and now with Catalina some
       users may have Music.app and others iTunes.
    
       During testing, I did notice that Application('com.apple.iTunes')
       actually returns Music.app on Catalina (yay backwards compatibility)
       but I'd rather be safe than sorry, and this fixes the exception for
       Vox, and any other apps I may add later.
    
    2) Supports Apple Music tracks
    
       This has been a pain in my ass for over a year now, since I started
       my free trial of Apple Music and then just decided to stick with it.
    
       Essentially, this boils down to the parenthesis after `currentTrack`,
       which change the type of object we're receiving. With the parenthesis
       we get the property, or the track object itself, and without the
       parenthesis we get an "object specifier", or a reference, which
       would always point to the currently playing track.
    
       There are more details about this here: https://github.com/JXA-Cookbook/JXA-Cookbook/wiki/iTunes#getting-the-current-track-an-example-of-specifier-vs-property-access

--- get-song.js
+++ get-song.js
@@ -1,20 +1,35 @@
 #!/usr/bin/osascript -l JavaScript
 
 function run(argv) {
-    if (Application('com.apple.iTunes').running()) {
-        var itunes = Application('com.apple.iTunes')
-        if (itunes.playerState() === 'playing') {
-            var track = itunes.currentTrack();
-            return track.name() + ' ' + track.artist();
-        }
-    }
 
-    if (Application('com.coppertino.Vox').running()) {
-        var vox = Application('com.coppertino.Vox');
-        if (vox.playerState() === 1) {
-            return vox.track() + ' ' + vox.artist();
+    try {
+        if (Application('com.apple.Music').running()) {
+            var musicApp = Application('com.apple.Music');
+            if (musicApp.playerState() === 'playing') {
+                var track = musicApp.currentTrack;
+                return track.name() + ' ' + track.artist();
+            }
         }
-    }
+    } catch (err) { /* user on pre-catalina */ }
+
+    try {
+        if (Application('com.apple.iTunes').running()) {
+            var itunes = Application('com.apple.iTunes');
+            if (itunes.playerState() === 'playing') {
+                var track = itunes.currentTrack;
+                return track.name() + ' ' + track.artist();
+            }
+        }
+    } catch (err) { /* user on catalina */ }
+
+    try {
+        if (Application('com.coppertino.Vox').running()) {
+            var vox = Application('com.coppertino.Vox');
+            if (vox.playerState() === 1) {
+                return vox.track() + ' ' + vox.artist();
+            }
+        }
+    } catch (err) { /* app may not be installed */ }
     
     // Easy to add more applications here
     
